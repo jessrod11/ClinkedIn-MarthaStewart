e651dd889dbbb91017865cafbd0d2b43ad5f72d5
John Achor <johnachor@gmail.com>
2018-10-01 21:22:19 -0500

    match michael suggestion re: namespace

--- ClinkedIn-MarthaStewart/TheClink/TheClink.cs
+++ ClinkedIn-MarthaStewart/TheClink/TheClink.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ClinkedIn_MarthaStewart.DataAccess
+namespace ClinkedIn_MarthaStewart.Clink
 {
     public class TheClink
     {
