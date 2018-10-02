0453619c5feb99df0edff2630f07c5004e81e803
michael <michael.clark1992@gmail.com>
2018-10-01 21:17:27 -0500

    added method to return all inmates in TheClink, added get request to filter inmates by interest

--- ClinkedIn-MarthaStewart/Controllers/InmatesController.cs
+++ ClinkedIn-MarthaStewart/Controllers/InmatesController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClinkedIn_MarthaStewart.Clink;
+using ClinkedIn_MarthaStewart.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +16,15 @@ namespace ClinkedIn_MarthaStewart.Controllers
         //Methods
         [HttpGet ("{id}/services"]
         // other code
-        
+        [HttpGet("interest")]
+        public ActionResult<IEnumerable<Inmate>> GetInmatesByInterest(string interest)
+        {
+            var clink = new TheClink();
+            var inmatesByInterest = clink.GetAllInmates().Where(inmate => inmate.Interests.Contains(interest));
+            return Ok(inmatesByInterest);
+
+
+        }
+
     }
 }
--- ClinkedIn-MarthaStewart/TheClink/TheClink.cs
+++ ClinkedIn-MarthaStewart/TheClink/TheClink.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ClinkedIn_MarthaStewart.TheClink
+namespace ClinkedIn_MarthaStewart.Clink
 {
     public class TheClink
     {
@@ -19,6 +19,11 @@ namespace ClinkedIn_MarthaStewart.TheClink
         internal Inmate GetById(int id)
         {
             return _theClink.FirstOrDefault(inmate => inmate.Id == id);
+        }
+
+        public IEnumerable<Inmate> GetAllInmates()
+        {
+            return _theClink;
         }
     }
 }
